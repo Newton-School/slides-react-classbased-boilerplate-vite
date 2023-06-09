707dff3ea572df9f3560dcfb85f77a6df9f037c9
Arnab Poddar <arnabpoddar@Arnabs-MacBook-Air.local>
2023-06-09 13:47:42 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+
+import React, { Component, useState } from "react";
+import '../styles/App.css';
+
+const App = props => {
+  const [slides, setSlides] = useState(props.slides);
+  const [index, setIndex] = useState(0);
+  // const { index, slides } = this.state;
+  return <>hello</>;
+};
+
+export default App;
+
